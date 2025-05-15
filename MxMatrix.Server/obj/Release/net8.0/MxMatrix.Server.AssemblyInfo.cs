5d106a3cc9e150a9566dcc1cbb459b0db32a219b
timwalkercs <timothywalker2000@gmail.com>
2025-05-15 17:55:18 -0400

    adjustments for publishing

--- MxMatrix.Server/obj/Release/net8.0/MxMatrix.Server.AssemblyInfo.cs
+++ MxMatrix.Server/obj/Release/net8.0/MxMatrix.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MxMatrix.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad52ef53e484f29e93e0fae32b3006bf4c22ad61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72e08f64057efe645b11291e8c22b51ddd1921b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MxMatrix.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MxMatrix.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
