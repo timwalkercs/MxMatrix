eae004ec814640336384ae2178495401cb91a5f2
timwalkercs <timothywalker2000@gmail.com>
2025-05-27 19:31:02 -0400

    Additions to learnmore page

--- mxmatrix.client/src/components/LearnMore.jsx
+++ mxmatrix.client/src/components/LearnMore.jsx
@@ -19,10 +19,10 @@ function LearnMore() {
 
             <section className="learn-section">
                 <div className="learn-header">
-                    <h3>How Do Mechanical Switches Work?</h3>
-                    <a href="https://mechanicalkeyboards.com/blogs/mk-101/what-is-a-mechanical-switch" target="_blank" rel="noopener noreferrer">
-                        Source
-                    </a>
+                    <h3>
+                        How Do Mechanical Switches Work?
+                    <a href="https://mechanicalkeyboards.com/blogs/mk-101/what-is-a-mechanical-switch" target="_blank" rel="noopener noreferrer">Source</a>
+                    </h3>
                 </div>
 
                 <p>
@@ -49,10 +49,11 @@ function LearnMore() {
 
             <section className="learn-section">
                 <div className="learn-header">
-                    <h3>Types of Switches</h3>
-                    <a href="https://www.mechkeybs.com/learn/keyboards-components/linear-vs-tactile-vs-clicky-switches/" target="_blank" rel="noopener noreferrer">
-                        Source
-                    </a>
+                    <h3>
+                        Types of Switches
+                        <a href="https://www.mechkeybs.com/learn/keyboards-components/linear-vs-tactile-vs-clicky-switches/" target="_blank" rel="noopener noreferrer">Source</a>
+
+                    </h3>
                 </div>
                 <ul>
                     <li><strong>Linear:</strong> Smooth keystroke without tactile bump or click (e.g., Cherry MX Red).</li>
@@ -79,11 +80,59 @@ function LearnMore() {
             </section>
 
             <section className="learn-section">
-                <h2>Why Choose Mechanical Switches?</h2>
+                <h2>Picking The Right Switch For You</h2>
                 <p>
-                    Mechanical switches are preferred by gamers, typists, and enthusiasts for their feel, speed, and longevity.
-                    Most are rated for tens of millions of keypresses.
+                    The ammount of switches on the market can be overwhelming for consumers.
+                    With the sheer mass of options available, it's important to understand what factors make one switch different from another.
                 </p>
+            </section>
+
+            <section className="learn-section">
+                <div className="learn-header">
+                    <h3>
+                        Points of Variation
+                    </h3>
+                </div>
+
+                <p>
+                    These specifications affect the general typing feel of a switch:
+                </p>
+                <ul>
+                    <li><strong>Operating/Actuation Force and Bottom-Out Force:</strong> Actuation force is the force needed to press a key and register an input. Bottom-out force is the force required for the stem to reach its total travel distance. Indicates how much resistance you'll feel when typing.</li>
+
+                    <li>
+                        <strong>Pre-Travel & Total Travel Distance:</strong> Pre-travel is how far a key must be pressed to actuate - shorter distances mean faster response. Total travel is the full depth a key can be pressed before bottoming out.
+                    </li>
+
+                    <li>
+                        <strong>Lubricant:</strong> Lubing improves switch smoothness. Many switches now come factory-lubed for better out-of-box performance. If you are dedicated hobbyist, or up for a challenge, you may decide to hand-lube your switches. But be attentive, consistency is key and too much can make a switch feel mushy.
+                    </li>
+                </ul>
+
+                <p>
+                    These specifications can affect the sound profile of a switch:
+                </p>
+                <ul>
+                    <li>
+                        <strong>Top Housing Material:</strong> The material used in the top housing can affect the sound of a switch when the spring returns and sends the stem back up.
+                        For example, a plastic like polycarbonate (PC) typically allows for a higher-pitched return sound.
+                    </li>
+
+                    <li>
+                        <strong>Bottom Housing Material:</strong> The bottom housing material affects how the switch sounds when it bottoms out.
+                        Nylon is a common material used for bottom housings and is associated with a deeper sound signature than something like polycarbonate.
+                    </li>
+
+                    <li>
+                        <strong>Dampeners (Silent Switches):</strong> Sometimes switches can be identified as 'silent' switches. This usually means that some form of dampener is used (rubber or a plastic mechanism) to mute the sound of a switch.
+                        Depending where the dampeners are placed, the silencing effect can nullify the sound of the bottom-out, return, or both. Silent switches can be linear or tactile and are a great choice for an office environment.
+                    </li>
+
+                    <li>
+                        <strong>Lubricant (again):</strong> Lubed switches can deepen the sound of a switch, nullify scratchiness, and eliminate annoying resonance that comes from some springs.
+                    </li>
+                </ul>
+
             </section>
 
             <div className="learnmore-button-wrapper">
